26a66bd23f2f4e3a16e8ea0243fa2afd532ed947
Yussufsherif <142413011+Yussufsherif@users.noreply.github.com>
2023-08-25 21:21:18 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
-	// start your task
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"data": "hello world"})
+	})
+	r.Run()
+}
+
+type postgres struct {
+	DB *sql.DB
+}
+
+func (a *postgres) CreateConnection() {
+	dbUname := "postgres"
+	dbPass := "yushkongpost"
+	dbHost := "localhost"
+	dbName := " clients"
+	connStr := fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=disable", dbUname, dbPass, dbHost, dbName)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	a.DB = db
 }
